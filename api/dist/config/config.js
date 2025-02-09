5cc50883c004c507499eb1b8eb9b6c4ee78c89e1
Luis Herrera <luis.herrera506@gmail.com>
2025-02-08 20:52:53 -0600

    Cleaning folders

--- api/dist/config/config.js
+++ api/dist/config/config.js
@@ -1,16 +1,24 @@
+"use strict";
+require('dotenv').config();
 module.exports = {
-  development: {
-    username: "postgres",
-    password: "root",
-    database: "choferes",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-  production: {
-    username: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
-    dialect: "postgres",
-  },
+    development: {
+        username: "postgres",
+        password: "root",
+        database: "choferes",
+        host: "127.0.0.1",
+        dialect: "postgres"
+    },
+    production: {
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        host: process.env.PGHOST,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    },
 };
