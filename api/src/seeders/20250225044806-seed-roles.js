0ba261b7c1dbf86a5f0ff3537e637640ed45f170
Luis Herrera <luis.herrera506@gmail.com>
2025-02-26 10:28:40 -0600

    Adding new role to seeder

--- api/src/seeders/20250225044806-seed-roles.js
+++ api/src/seeders/20250225044806-seed-roles.js
@@ -5,7 +5,8 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert("roles", [
       { id: 1, name: "Admin", createdAt: new Date(), updatedAt: new Date() },
-      { id: 2, name: "User", createdAt: new Date(), updatedAt: new Date() },
+      { id: 2, name: "Coordinator", createdAt: new Date(), updatedAt: new Date() },
+      { id: 3, name: "User", createdAt: new Date(), updatedAt: new Date() },
     ]);
   },
 
